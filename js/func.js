c2a445b1b03642f6c85d7bedb73322ca6d9b93bd
Serg <tai2007@rambler.ru>
2020-08-10 18:54:50 +0400

    function

---
+++ js/func.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let num=20;
+
+function showFirstMessage(){
+   let num=10;
+}
+
+// showFirstMessage();
+// console.log(num);
+
+function calc(a,b){
+  return(a + b); 
+  //console.log('fddfghfd'); 
+}
+
+console.log(calc(3,2));
+
+const a = ret();
+console.log(a);
+
+function ret(){
+    let num = 50;
+    return num;
+}
+
+
+
+const logger = function(){
+ console.log ('Hello');
+};
+
+logger();
+
+
+const calc1 = (a, b) => {return (a + b); };
+console.log(calc1(3,2));
+
+
+
+
+
+
