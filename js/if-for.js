809e7b647a1afc60f0491a59000670e13202ec28
Serg <tai2007@rambler.ru>
2020-08-10 16:07:56 +0400

    practic_2

---
+++ js/if-for.js
@@ -0,0 +1,49 @@
+"use strict";
+//const num = 50;
+// if (num < 49) {
+//   console.log("error");  
+// } else if (num>100) {
+//   console.log("Много");  
+// } else {
+//    console.log("Ok!");    
+// }
+
+// (num === 50) ? console.log("Ok!") : console.log("error"); 
+
+
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Много');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;    
+// }
+
+let num = 50;
+//  while (num <= 55) {
+//     console.log(num);
+//     num++;
+//  }
+
+//  do {
+//      console.log(num);
+//      num++;
+//  }
+//  while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6){
+       //break;  
+       continue;
+    }
+   console.log(i);
+   
+}
