ed6f3ba9d3d147b35bfce2096ac76af491b695e2
Serg <tai2007@rambler.ru>
2020-08-11 09:36:54 +0400

    practic_3

---
+++ js/practic_3.js
@@ -0,0 +1,88 @@
+"use strict";
+
+let numberOfFilms;
+
+function start(){
+  numberOfFilms= +prompt("Сколько фильмов вы уже посмотрели?","");
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms= +prompt("Сколько фильмов вы уже посмотрели?","");     
+  }
+}
+
+//start();
+
+
+const personalMovieDB={
+       count: numberOfFilms,
+       movies: {},
+       actors: {},
+       genres: [],
+       privat: false 
+      };
+// const a = prompt("Один из последних просмотренных фильмов",""),
+//       b = prompt("На сколько оцените его?",""),
+//       c = prompt("Один из последних просмотренных фильмов",""),
+//       d = prompt("На сколько оцените его?","");
+
+
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов",""),
+              b = prompt("На сколько оцените его?","");
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a]=b; 
+            console.log('done');    
+        } else {
+            console.log('error');
+            i--;
+        }  
+    }
+    
+}
+
+//rememberMyFilms();
+
+
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+     } else if (personalMovieDB.count >= 10 && personalMovieDB.count<30) {
+         console.log('Вы классический зритель'); 
+     } else if (personalMovieDB.count >= 30) {
+         console.log('Вы киноман'); 
+     } else {
+         console.log('Ошибка'); 
+     }
+}
+
+//detectPersonalLevel();
+
+function showMyDB(){
+ if (!personalMovieDB.privat){
+  console.log(personalMovieDB);
+ }  else {
+  console.log("Правда");   
+ }
+
+}
+
+showMyDB();
+
+
+function writeYourGenres(){
+  for (let i = 0; i < 3; i++ ){
+   personalMovieDB.genres[i] = 
+        prompt(`Ваш любимый жанр под номером ${i+1}`,"");
+ }
+}
+
+writeYourGenres();
+console.log(personalMovieDB);
+
+
+
+
+//console.log (personalMovieDB);
