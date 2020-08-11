b31926e6ea1daf2e9ff3f32d066ad1ace0b17561
Serg <tai2007@rambler.ru>
2020-08-11 08:05:24 +0400

    str_match

---
+++ js/str_digit.js
@@ -0,0 +1,30 @@
+"use sirict";
+
+//let str="teSt";
+//console.log(str.length);
+//const arr =[1,2,3];
+//console.log(arr.length);
+
+// console.log(str[2]);
+// str[2] = 'd';
+// 
+
+// const fruit = "SOMe fruit";
+// console.log(fruit.indexOf("fruito"));
+// if (fruit.toLowerCase().indexOf("om") < 0){
+//   console.log("Нет");
+// } else{
+//   console.log("Да");  
+// }
+
+// const logg = "Hello world";
+// console.log(logg.substr(6,5));
+// console.log(logg.slice(6,10));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "122px";
+console.log(typeof(parseInt(test)));
+console.log(parseFloat(test));
