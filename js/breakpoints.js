746d0094da01539706c19b4ff65d451c5d7d9b67
Serg <tai2007@rambler.ru>
2020-08-12 19:25:17 +0400

    debug

---
+++ js/breakpoints.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function hello(){
+ console.log("Hello world");   
+}
+
+hello();
+
+function hi(){
+ console.log("Say hi");   
+ debugger;
+}
+
+hi();
+
+function func(){
+ console.log("new");   
+}
+
+func();
