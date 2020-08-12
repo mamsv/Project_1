7a44bdb3eb35114aa8b63b39c42abbca8736370b
Serg <tai2007@rambler.ru>
2020-08-12 10:56:30 +0400

    opp

---
+++ js/opp.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+
+const soldier ={
+     health: 400,
+     armor: 100,
+     sayHello: function (){
+         console.log("Hello");
+     }
+};
+
+const John = Object.create(soldier);
+John.sayHello();
