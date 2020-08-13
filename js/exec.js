15cb625c22baee02e1a97016a1ac72a79773cf07
Serg <tai2007@rambler.ru>
2020-08-13 09:13:40 +0400

    exec

---
+++ js/exec.js
@@ -0,0 +1,32 @@
+"user strict";
+
+// let x = 5; 
+// let y = ++x;
+
+// alert(y);
+
+//let y = 1; let x = y = 2; alert(x);
+
+// 
+
+// alert("51234"[0]);
+
+// if (2 && 1 && null && 0 && undefined){
+//     alert ("true");
+// }  else{
+//     alert ("false");
+// }
+
+// alert ((1 && 2));
+
+
+//alert (null || 2 && 3 || 4);
+const a = 1;
+const b = 1;
+
+if (a === b){
+    alert ("true");
+}
+ else{
+     alert ("false");
+ }
